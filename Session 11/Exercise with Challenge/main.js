17fcdf93858d627ab362e34fdbcb050e9f6d793e
cstriker421 <cstriker421@gmail.com>
2025-02-14 21:08:58 +0000

    Fix up main.js of Session 11

--- Session 11/Exercise with Challenge/main.js
+++ Session 11/Exercise with Challenge/main.js
@@ -1,41 +1,41 @@
 class Animal {
     constructor(name) {
-      this.name = name;
+        this.name = name;
     }
-  
+
     speak() {
-      return `${this.name} makes a noise.`;
+        return `${this.name} makes a noise.`;
     }
-  
+
     static info() {
-      return "I am an animal class.";
+        return "I am an animal class.";
     }
-  }
-  
-  class Dog extends Animal {
+}
+
+class Dog extends Animal {
     constructor(name) {
-      super(name);
+        super(name);
     }
   
     speak() {
-      return `${super.speak()} ${this.name} barks!`;
+        return `${super.speak()} ${this.name} barks!`;
     }
-  }
-  
-  class Cat extends Animal {
+}
+
+class Cat extends Animal {
     constructor(name) {
-      super(name);
+        super(name);
     }
-  
+
     speak() {
-      return "Meow!";
+        return "Meow!";
     }
-  }
+}
+
+const dog = new Dog("Rex");
+console.log(dog.speak()); // "Rex makes a noise. Rex barks!"
+
+const cat = new Cat("Whiskers");
+console.log(cat.speak()); // "Meow!"
   
-  const dog = new Dog("Rex");
-  console.log(dog.speak()); // "Rex makes a noise. Rex barks!"
-  
-  const cat = new Cat("Whiskers");
-  console.log(cat.speak()); // "Meow!"
-  
-  console.log(Animal.info()); // "I am an animal class."  
+console.log(Animal.info()); // "I am an animal class."
